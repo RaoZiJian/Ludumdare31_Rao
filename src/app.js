1a390317a0223899f962aa557b05a48429fb7f64
raozijian <sikenrao@gmail.com>
2014-12-07 23:35:49 +0800

    3rd commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-var g_groundHight = 57;
-var g_runnerStartX = 80;
+var currentLayer;
 
 var BattleLayer = cc.Layer.extend({
     space:null,
@@ -8,19 +7,30 @@ var BattleLayer = cc.Layer.extend({
     BallShape:null,
     winSize:null,
     board:null,
+    boardShape:null,
     ballDistance:25,
     ballScale:0.5,
     ballAmount:20,
-    bottomWallHeight:100,
+    bottomWallHeight:135,
+    touchBeginPos:null,
+    touchEndedPos:null,
+    ifTouchFlag:false,
+    basket:null,
+    crutch:null,
+    particle:null,
+    score:0,
+    scoreLabel:null,
 
     onEnter:function(){
 
         this.winSize = cc.director.getWinSize();
         this._super();
+        this.initBackground();
         this.initPhysics();
         this.initBall();
-        this.initBoard();
-        this.setupDebugNode();
+        //this.setupDebugNode();
+        this.createBoardBody();
+        this.initTouchEvent();
 
         this.scheduleUpdate();
     },
@@ -31,49 +41,153 @@ var BattleLayer = cc.Layer.extend({
     },
 
     initBall:function(){
-
-        this.ball = cc.PhysicsSprite.create(res.PNG_PINGPONG);
+        this.ball = cc.PhysicsSprite.create(res.PNG_FACE1);
+        this.ball.setScale(0.5);
 
         this.BallBody = new cp.Body(50,1);
         this.space.addBody(this.BallBody);
 
         this.BallShape = new cp.CircleShape(this.BallBody,14,cp.v(0,0))
         this.BallShape.setFriction(0.5)
-        this.BallShape.setElasticity(0.5);
-        this.BallShape.setCollisionType(1);
+        this.BallShape.setElasticity(1.2);
+        this.BallShape.setCollisionType(10);
         this.space.addShape(this.BallShape);
         this.ball.setBody(this.BallBody);
-        this.ball.setPosition(this.winSize.width/2,this.winSize.height/2);
+        this.ball.setPosition(this.winSize.width/2,this.winSize.height/2+400);
         this.addChild(this.ball);
+
+        this.space.addCollisionHandler(10, 0, this.ballHitCallback.bind(this));
+        this.space.addCollisionHandler(10, 2, this.ballHitCallback2.bind(this));
+        this.space.addCollisionHandler(10, 100, this.ballHitCallback3.bind(this));
 
     },
 
-    initBoard:function(){
+    ballHitCallback:function(arbiter, space){
 
-        this.board = cc.PhysicsSprite.create(res.PNG_JOINT);
-        this.board.setScaleX(5);
-        var contentSize = this.board.getContentSize();
-        var crutchBody = new cp.Body(1000,1);
-        //crutchBody.v_limit = 0.5;
-        this.space.addBody(crutchBody);
+        cc.audioEngine.playEffect(res.WAV_EFFECT1);
+        return true;
+    },
 
-        var crutchShape = new cp.BoxShape(crutchBody,contentSize.width*5,contentSize.height);
+    ballHitCallback2:function(arbiter, space){
+
+        cc.audioEngine.playEffect(res.WAV_EFFECT2);
+        return true;
+    },
+
+    ballHitCallback3:function(arbiter, space){
+
+        currentLayer.score+=10;
+        this.scoreLabel.setString("Score : " + currentLayer.score);
+        return true;
+    },
+
+    createBoardBody:function(){
+
+        currentLayer.board = cc.PhysicsSprite.create(res.PNG_JOINT);
+        var contentSize = currentLayer.board.getContentSize();
+        var boardBody = new cp.Body(Infinity,Infinity);
+        boardBody.nodeIdleTime = Infinity;
+
+        boardBody.v_limit = 0.5;
+        currentLayer.board.setBody(boardBody);
+        //currentLayer.addChild(this.board);
+        //currentLayer.board.setAnchorPoint(cc.p(0,0));
+        //currentLayer.space.addBody(boardBody);
+    },
+
+    createBoardShape:function(touch){
+
+        var body = currentLayer.board.getBody();
+        var bodyShape = currentLayer.boardShape;
+        var startPos = currentLayer.touchBeginPos;
+        var movePos = touch.getLocation();
+
+        var yMinus = Math.abs(startPos.y-movePos.y);
+        var xMinus = Math.abs(startPos.x-movePos.x);
+
+        if(currentLayer.boardShape!=null) {
+            body.removeShape(bodyShape);
+            currentLayer.space.removeShape(bodyShape);
+        }
+        var width = Math.sqrt((xMinus*xMinus+yMinus*yMinus));
+
+        var crutchShape = new cp.SegmentShape(body,startPos,movePos,10);
         crutchShape.setFriction(1);
-        crutchShape.setElasticity(0.2);
+        crutchShape.setElasticity(1);
         crutchShape.setCollisionType(2);
-        this.space.addShape(crutchShape);
-        this.board.setBody(crutchBody);
-        this.addChild(this.board);
-        this.board.setPosition(cc.p(this.winSize.width/2,this.winSize.height/2));
+        currentLayer.boardShape=crutchShape;
+        currentLayer.space.addShape(crutchShape);
+        currentLayer.crutch.setScaleX(width/28);
+        currentLayer.crutch.setScaleY(0.5);
 
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-            onMouseMove:function(event) {
-                if (this.board != null){
-                    this.board.setPosition(event.getLocation());
+        if(startPos.y>movePos.y&&startPos.x<movePos.x) {
+
+            var atan = Math.atan(yMinus/xMinus);
+            var rotation = atan/(Math.PI*2/360);
+            currentLayer.crutch.setRotation(rotation);
+        }else if(startPos.y>movePos.y&&startPos.x>movePos.x){
+
+            var atan = Math.atan(yMinus/xMinus);
+            var rotation = 180 - atan/(Math.PI*2/360);
+            currentLayer.crutch.setRotation(rotation);
+        }else if(startPos.y<movePos.y&&startPos.x>movePos.x){
+
+            var atan = Math.atan(yMinus/xMinus);
+            var rotation = atan/(Math.PI*2/360)-180;
+            currentLayer.crutch.setRotation(rotation);
+        }else if(startPos.y<movePos.y&&startPos.x<movePos.x){
+            var atan = Math.atan(yMinus/xMinus);
+            var rotation = -atan/(Math.PI*2/360);
+            currentLayer.crutch.setRotation(rotation);
+        }
+        currentLayer.addChild(currentLayer.crutch);
+
+    },
+
+    initTouchEvent:function(){
+
+        var touchListener = cc.EventListener.create({
+            event:cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches:true,
+            onTouchBegan: function (touch,event) {
+
+                if(currentLayer.ifTouchFlag)
+                    return;
+
+                currentLayer.ifTouchFlag = true;
+                currentLayer.touchBeginPos = touch.getLocation();
+                if(currentLayer.crutch!=null) {
+                    currentLayer.crutch.removeFromParent();
                 }
+                currentLayer.crutch = cc.Sprite.create(res.PNG_CRUTCH);
+                currentLayer.crutch.setAnchorPoint(cc.p(0, 0.5));
+                currentLayer.crutch.setPosition(touch.getLocation());
+                currentLayer.particle = cc.ParticleSystem.create(res.PARTICLE);
+                currentLayer.particle.setScale(0.7);
+                currentLayer.particle.setPosition(touch.getLocation());
+                currentLayer.addChild(currentLayer.particle);
+
+                cc.log("the touchX begin is " + touch.getLocationX() + "the touchY begin is " + touch.getLocationY());
+
+                return true;
+            },
+
+            onTouchMoved: function (touch, event) {
+                currentLayer.particle.setPosition(touch.getLocation());
+            },
+
+            onTouchEnded:function(touch,event){
+                if(currentLayer.ifTouchFlag) {
+                    currentLayer.createBoardShape(touch);
+                }
+                currentLayer.particle.setPosition(touch.getLocation());
+                currentLayer.particle.removeFromParent();
+                currentLayer.particle=null;
+                currentLayer.ifTouchFlag = false;
+                cc.log("the touchX begin is " + touch.getLocationX() + "the touchY begin is " + touch.getLocationY());
             }
-        },this);
+        });
+        cc.eventManager.addListener(touchListener, this);
     },
 
     initPhysics:function() {
@@ -82,22 +196,82 @@ var BattleLayer = cc.Layer.extend({
 
         var wallBottom = new cp.SegmentShape(this.space.staticBody,cp.v(0,0), cp.v(this.winSize.width, 0),this.bottomWallHeight);
         wallBottom.setCollisionType(0);
-        wallBottom.setElasticity(0.5);
+        wallBottom.setElasticity(0.7);
         wallBottom.setFriction(1);
 
-        var wallLeft = new cp.SegmentShape(this.space.staticBody,cp.v(0,0), cp.v(0, this.winSize.height),10);
+        var wallLeft = new cp.SegmentShape(this.space.staticBody,cp.v(0,0), cp.v(0, 100000000),10);
         wallLeft.setCollisionType(0);
-        wallLeft.setElasticity(0);
+        wallLeft.setElasticity(0.5);
         wallLeft.setFriction(1);
 
-        var wallRight = new cp.SegmentShape(this.space.staticBody,cp.v(this.winSize.width,0), cp.v(this.winSize.width, this.winSize.height),10);
+        var wallRight = new cp.SegmentShape(this.space.staticBody,cp.v(this.winSize.width,0), cp.v(this.winSize.width, 100000000),10);
         wallRight.setCollisionType(0);
-        wallRight.setElasticity(0);
+        wallRight.setElasticity(0.5);
         wallRight.setFriction(1);
+
+        var bridge = new cp.SegmentShape(this.space.staticBody,cp.v(10.6,369), cp.v(281, 132),10);
+        bridge.setCollisionType(0);
+        bridge.setElasticity(0.5);
+        bridge.setFriction(1);
+
+        var basket = new cp.SegmentShape(this.space.staticBody,cp.v(920,560), cp.v(920, 404),30);
+        basket.setCollisionType(0);
+        basket.setElasticity(0.5);
+        basket.setFriction(1);
+
+        var basket2 = new cp.SegmentShape(this.space.staticBody,cp.v(869.6,458.3), cp.v(893, 458.3),5);
+        basket2.setCollisionType(100);
+        basket2.setElasticity(0.5);
+        basket2.setFriction(1);
+
+        var basketCrutch = new cp.SegmentShape(this.space.staticBody,cp.v(1007,575), cp.v(1009, 141),10);
+        basketCrutch.setCollisionType(0);
+        basketCrutch.setElasticity(0.5);
+        basketCrutch.setFriction(1);
+
+        var jumpLayer = new cp.SegmentShape(this.space.staticBody,cp.v(470,143), cp.v(470-115, 143),8);
+        jumpLayer.setCollisionType(0);
+        jumpLayer.setElasticity(1);
+        jumpLayer.setFriction(1);
 
         this.space.addStaticShape(wallBottom);
         this.space.addStaticShape(wallLeft);
         this.space.addStaticShape(wallRight);
+        this.space.addStaticShape(bridge);
+        this.space.addStaticShape(basket);
+        this.space.addStaticShape(basketCrutch);
+        this.space.addStaticShape(basket2);
+        this.space.addStaticShape(jumpLayer);
+    },
+
+    initBackground:function(){
+
+        var bg = cc.Sprite.create(res.PNG_BG);
+        bg.setPosition(cc.p(0,0));
+        bg.setAnchorPoint(cc.p(0,0));
+        this.addChild(bg,0);
+
+        this.basket = cc.Sprite.create(res.PNG_BASKET);
+        this.basket.setPosition(cc.p(900,470));
+        this.basket.setAnchorPoint(cc.p(1,1));
+        this.addChild(this.basket,100);
+
+        var logo = cc.Sprite.create(res.PNG_LOGO);
+        logo.setAnchorPoint(cc.p(0,1));
+        logo.setPosition(0,this.winSize.height-100);
+        this.addChild(logo);
+
+        var jumpLayer = cc.Sprite.create(res.PNG_JUMP);
+        jumpLayer.setAnchorPoint(1,0.5);
+        jumpLayer.setPosition(470,143);
+        this.addChild(jumpLayer);
+
+        this.scoreLabel = new cc.LabelTTF("Score : " + currentLayer.score);
+        this.scoreLabel.setFontSize(50);
+        this.scoreLabel.setAnchorPoint(0,0.5);
+        this.scoreLabel.setColor(cc.color(255,0,0,0));
+        this.scoreLabel.setPosition(cc.p(0,this.winSize.height-200));
+        this.addChild(this.scoreLabel);
     },
 
     update:function(dt){
@@ -110,6 +284,7 @@ var BattleScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
         var layer = new BattleLayer();
+        currentLayer = layer;
         this.addChild(layer);
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -1,6 +1,19 @@
 var res = {
     PNG_PINGPONG:"res/pingpong.png",
-    PNG_JOINT:"res/Joint.png"
+    PNG_JOINT:"res/Joint.png",
+    PNG_BG:"res/background.png",
+    PNG_BASKET:"res/basket.png",
+    PNG_CRUTCH:"res/board.png",
+    PNG_LOGO:"res/Logo.png",
+    PNG_JUMP:"res/jumplayer.png",
+    PNG_FACE1:"res/foreFace1.png",
+    PNG_FACE2:"res/foreFace2.png",
+    PNG_FACE3:"res/foreFace3.png",
+
+    PARTICLE:"res/particle.plist",
+
+    WAV_EFFECT1:"res/Hit_Hurt.wav",
+    WAV_EFFECT2:"res/Randomize.wav"
 };
 
 var g_resources = [];
